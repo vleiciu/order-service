a2c26904a3554741241fc8374fd407267d7b7f68
thaimore <volodea.leyciu@gmail.com>
2023-12-07 11:38:22 +0200

    Nexus and kuber configs added

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM eclipse-temurin:17-jdk-alpine
 VOLUME /tmp
 EXPOSE 8081
-ARG JAR_FILE=target/OrderService-0.0.1-SNAPSHOT.jar
-ADD ${JAR_FILE} OrderService-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java","-jar","/OrderService-0.0.1-SNAPSHOT.jar"]
+ARG LOCATION
+ENV LOCATION ${LOCATION}
+ARG JAR_FILE
+ENV JAR_FILE ${JAR_FILE}
+ADD target/${JAR_FILE} ${JAR_FILE}
+CMD java -jar /${JAR_FILE} --spring.config.location=classpath:/${LOCATION}
--- src/main/java/com/org/os/integration/AmqpAdapter.java
+++ src/main/java/com/org/os/integration/AmqpAdapter.java
@@ -2,20 +2,25 @@ package com.org.os.integration;
 
 import com.org.os.orchestration.OrchestrationApiFactory;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import static com.org.ma.utils.Constants.ORDER_CHANNEL;
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AmqpAdapter extends RouteBuilder {
+
+    @Value(value = "${spring.kafka.consumer.bootstrap-servers}")
+    private String brokerAddress;
 
     private OrchestrationApiFactory factory;
 
     @Override
     public void configure() {
-        from("kafka:%s?brokers=172.18.0.1:9092".formatted(ORDER_CHANNEL))
+        from("kafka:%s?brokers=%s".formatted(ORDER_CHANNEL, brokerAddress))
                 .routeId("order-service")
                 .process(factory);
     }
